b05b4a8de56eb78991e97d92a8adf1bdaf7abc43
juanma0089 <juanma0089@hotmail.com>
2023-01-12 14:03:00 +0100

    retoque conexion pdo

--- conexionPDO.php
+++ conexionPDO.php
@@ -7,10 +7,12 @@ function connectPDO(){
         $conexion = new PDO(DSN, USER, PASSWORD);
         $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         // echo "te has conectado al a bbdd de lol";
-
+        return $conexion;
     } catch (PDOException $e) {
         echo "Falló la conexión " . $e->getMessage();
     }
+
 }
+
 ?>
 
